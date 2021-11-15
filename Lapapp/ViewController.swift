bb8552c6573d2e503a7cf7b5759f80a9a6ab7d88
borisbolsh <bolsh.bo@yandex.ru>
2021-11-15 13:49:15 +0300

    initial commit

---
+++ Lapapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lapapp
+//
+//  Created by Boris Bolshakov on 15.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
