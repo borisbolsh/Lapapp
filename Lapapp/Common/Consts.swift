eb8e35e821bbaf0af1081f4bdb0f43f2bc8b1a96
borisbolsh <bolsh.bo@yandex.ru>
2021-11-16 19:51:26 +0300

    started adding structure

---
+++ Lapapp/Common/Consts.swift
@@ -0,0 +1,20 @@
+//
+//  Consts.swift
+//  Lapapp
+//
+//  Created by Boris Bolshakov on 16.11.21.
+//
+
+import Foundation
+
+final class Consts {
+    
+    struct APIKeys {
+        static let footballDataApiKey = "API-KEY"
+    }
+    
+    enum Links {
+           static let footballBaseUrl = "football-data.org"
+    }
+    
+}
---
+++ Lapapp/Services/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  Lapapp
+//
+//  Created by Boris Bolshakov on 16.11.21.
+//
+
+import Foundation
+
+class NetworkService {
+    
+    
+}
